ba9bd0202c760e09e4b56eb075050643fd8dca00
mihinduranasinghe <mihindu.21523@gmail.com>
2020-11-18 18:01:16 +0530

    custom js actions check errors handling

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -2,13 +2,14 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 try {
-    // throw new console.error("some error simulating");
+    throw new console.error("some error simulating");
     const name = core.getInput("who-to-greet");
     console.log(`Hello ${name}`);
 
     const time = new Date();
     core.setOutput("time", time.toTimeString());
 
+    // print the github object
     console.log(JSON.stringify(github, null, "\t"));
 } catch (error) {
     core.setFailed(error.message);
