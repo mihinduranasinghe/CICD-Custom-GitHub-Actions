1bbdf3aadc7aefcb40c0e1f76ecefa6e7d5b610b
mihinduranasinghe <mihindu.21523@gmail.com>
2020-11-18 18:41:19 +0530

    more on core package

--- .github/actions/hello/dist/index.js
+++ .github/actions/hello/dist/index.js
@@ -10,6 +10,7 @@ const github = __webpack_require__(438);
 
 try {
     // throw new console.error("some error simulating");
+
     const name = core.getInput("who-to-greet");
     console.log(`Hello ${name}`);
 
@@ -17,7 +18,18 @@ try {
     core.setOutput("time", time.toTimeString());
 
     // print the github object
-    console.log(JSON.stringify(github, null, "\t"));
+    // console.log(JSON.stringify(github, null, "\t"));
+
+    core.startGroup("Logging github object");
+    onsole.log(JSON.stringify(github, null, "\t"));
+    core.endGroup();
+
+    //More about core package
+    core.debug("Debug message");
+    core.warning("Warning message"); // This will be displayed in yello color in logs
+    core.error("Error message"); // This will be displayed in red color in logs
+    core.setSecret("This text will be displayed as *** in logs");
+    core.exportVariable("HELLO", "hello"); //Exporting environment variables
 } catch (error) {
     core.setFailed(error.message);
 }
