8f4e0ba67ace84e793f4b7db88447b49a18750aa
mihinduranasinghe <mihindu.21523@gmail.com>
2020-11-21 00:49:58 +0530

    php action with docker images bugfixed

--- .github/actions/slack-docker/script.php
+++ .github/actions/slack-docker/script.php
@@ -3,53 +3,82 @@
 require_once 'vendor/autoload.php';
 Requests::register_autoloader();
 
-#for getting inputs from workflow
-// var_dump($argv);
-// var_dump($_ENV);
+#for getting inputs from
+var_dump($argv);
+var_dump($_ENV);
 
-$response = Requests::post($_ENV['INPUT_SLACK_WEBHOOK'],
-    array(
-        'Content-type'=> application/json
-    ),
-    json_encode(array(
-        array(
-            array(
-                'type' => 'setion',
-                'text'=>array(
-                    'type' => 'mrkdwn',
-                    'text' => $_ENV['INPUT_MESSAGE'],
-                    # $_ENV[you can populate any github object env variables also] 
-                ),
-            ),
+// $response = Requests::post("https://hooks.slack.com/services/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",
+//     array(
+//         'Content-type'=> application/json
+//     ),
+//     json_encode(array(
+//         'text' => 'Some message from php script'
+//     ))
 
-    //         array(
-    //             'type' => 'setion',
-    //             'fields'=> array(
-    //                 array(
-    //                     'type' => 'mrkdwn',
-    //                     'text' => "*Repository:*\n{$_ENV['GITHUB_REPOSITORY']}",                          
-    //                     ),
-    //                 array(
-    //                     'type' => 'mrkdwn',
-    //                     'text' => "*Repository:*\n{$_ENV['GITHUB_EVENT_NAME']}",                          
-    //                     ),
-    //                 array(
-    //                     'type' => 'mrkdwn',
-    //                     'text' => "*Repository:*\n{$_ENV['GITHUB_SHA']}",                          
-    //                     ),             
-    //     ),
-    //   ),
-    ),
-   ),
- ),
+// );
+
+// var_dump($response);
+
+// if(!response -> success){
+//     echo $response->body;
+// }
+
+
+
+// ----
+
+// <?php
+
+// require_once 'vendor/autoload.php';
+// Requests::register_autoloader();
+
+// #for getting inputs from workflow
+// // var_dump($argv);
+// // var_dump($_ENV);
+
+// $response = Requests::post($_ENV['INPUT_SLACK_WEBHOOK'],
+//     array(
+//         'Content-type'=> application/json
+//     ),
+//     json_encode(array(
+//         array(
+//             array(
+//                 'type' => 'setion',
+//                 'text'=>array(
+//                     'type' => 'mrkdwn',
+//                     'text' => $_ENV['INPUT_MESSAGE'],
+//                     # $_ENV[you can populate any github object env variables also] 
+//                 ),
+//             ),
+
+//     //         array(
+//     //             'type' => 'setion',
+//     //             'fields'=> array(
+//     //                 array(
+//     //                     'type' => 'mrkdwn',
+//     //                     'text' => "*Repository:*\n{$_ENV['GITHUB_REPOSITORY']}",                          
+//     //                     ),
+//     //                 array(
+//     //                     'type' => 'mrkdwn',
+//     //                     'text' => "*Repository:*\n{$_ENV['GITHUB_EVENT_NAME']}",                          
+//     //                     ),
+//     //                 array(
+//     //                     'type' => 'mrkdwn',
+//     //                     'text' => "*Repository:*\n{$_ENV['GITHUB_SHA']}",                          
+//     //                     ),             
+//     //     ),
+//     //   ),
+//     ),
+//    ),
+//  ),
   
-);
+// );
 
-echo "::group::Some expendable slack response\n";
-var_dump($response);
-echo "::endgroup::\n";
+// echo "::group::Some expendable slack response\n";
+// var_dump($response);
+// echo "::endgroup::\n";
 
-if(!$response -> success){
-    echo $response->body;
-    exit(1);
-}
+// if(!$response -> success){
+//     echo $response->body;
+//     exit(1);
+// }
