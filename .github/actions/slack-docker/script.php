01d5a6ea4936c7aed95eca6e65d00d40d072fae9
mihinduranasinghe <mihindu.21523@gmail.com>
2020-11-21 01:06:56 +0530

    php action with docker images improved

--- .github/actions/slack-docker/script.php
+++ .github/actions/slack-docker/script.php
@@ -21,23 +21,23 @@ $response = Requests::post($_ENV['INPUT_SLACK_WEBHOOK'],
                         # $_ENV[you can populate any github object env variables also] 
                     ),
                 ),
-                array(
-                    'type' => 'setion',
-                    'fields'=> array(
-                        array(
-                            'type' => 'mrkdwn',
-                            'text' => "*Repository:*\n{$_ENV['GITHUB_REPOSITORY']}",                          
-                            ),
-                        array(
-                            'type' => 'mrkdwn',
-                            'text' => "*Repository:*\n{$_ENV['GITHUB_EVENT_NAME']}",                          
-                            ),
-                        array(
-                            'type' => 'mrkdwn',
-                            'text' => "*Repository:*\n{$_ENV['GITHUB_SHA']}",                          
-                            ),             
-                    ),
-                ),
+                // array(
+                //     'type' => 'setion',
+                //     'fields'=> array(
+                //         array(
+                //             'type' => 'mrkdwn',
+                //             'text' => "*Repository:*\n{$_ENV['GITHUB_REPOSITORY']}",                          
+                //             ),
+                //         array(
+                //             'type' => 'mrkdwn',
+                //             'text' => "*Repository:*\n{$_ENV['GITHUB_EVENT_NAME']}",                          
+                //             ),
+                //         array(
+                //             'type' => 'mrkdwn',
+                //             'text' => "*Repository:*\n{$_ENV['GITHUB_SHA']}",                          
+                //             ),             
+                //     ),
+                // ),
             )   
         )
     ),
@@ -58,60 +58,3 @@ if(!$response -> success){
 
 
 
-// ----
-
-// <?php
-
-// require_once 'vendor/autoload.php';
-// Requests::register_autoloader();
-
-// #for getting inputs from workflow
-// // var_dump($argv);
-// // var_dump($_ENV);
-
-// $response = Requests::post($_ENV['INPUT_SLACK_WEBHOOK'],
-//     array(
-//         'Content-type'=> application/json
-//     ),
-//     json_encode(array(
-//         array(
-//             array(
-//                 'type' => 'setion',
-//                 'text'=>array(
-//                     'type' => 'mrkdwn',
-//                     'text' => $_ENV['INPUT_MESSAGE'],
-//                     # $_ENV[you can populate any github object env variables also] 
-//                 ),
-//             ),
-
-//             array(
-//                 'type' => 'setion',
-//                 'fields'=> array(
-//                     array(
-//                         'type' => 'mrkdwn',
-//                         'text' => "*Repository:*\n{$_ENV['GITHUB_REPOSITORY']}",                          
-//                         ),
-//                     array(
-//                         'type' => 'mrkdwn',
-//                         'text' => "*Repository:*\n{$_ENV['GITHUB_EVENT_NAME']}",                          
-//                         ),
-//                     array(
-//                         'type' => 'mrkdwn',
-//                         'text' => "*Repository:*\n{$_ENV['GITHUB_SHA']}",                          
-//                         ),             
-//         ),
-//       ),
-//     ),
-//    ),
-//  ),
-  
-// );
-
-// echo "::group::Some expendable slack response\n";
-// var_dump($response);
-// echo "::endgroup::\n";
-
-// if(!$response -> success){
-//     echo $response->body;
-//     exit(1);
-// }
